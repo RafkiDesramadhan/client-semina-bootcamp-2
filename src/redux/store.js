67ea983e4c47605da60610f5c652341e4d570c8f
Andrian TEA - Pusat <rafki.desramadhan@gmail.com>
2026-07-23 20:48:00 +0700

    Updated Redux Devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import {
   combineReducers,
   legacy_createStore as createStore,
   applyMiddleware,
-  compose,
 } from "redux";
 
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
 import authReducer from "./auth/reducer";
 import categoriesReducer from "./categories/reducer";
 import notifReducer from "./notif/reducer";
@@ -14,8 +15,6 @@ import paymentsReducer from "./payments/reducer";
 import eventsReducer from "./events/reducer";
 import listsReducer from "./lists/reducer";
 import ordersReducer from "./orders/reducer";
-
-const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
   auth: authReducer,
@@ -30,7 +29,7 @@ const rootReducers = combineReducers({
 
 const store = createStore(
   rootReducers,
-  composerEnhancer(applyMiddleware(thunk)),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 export default store;
